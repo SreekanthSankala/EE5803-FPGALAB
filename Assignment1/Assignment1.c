ee3fa0f4ec921f9b6d732f2759365242d1dd3f49
Sreekanth <EE20RESCH11011@iith.ac.in>
2022-01-15 12:00:07 +0530

    Update Assignment1.c

--- Assignment1/Assignment1.c
+++ Assignment1/Assignment1.c
@@ -18,7 +18,7 @@ bool nand4(bool x, bool y, bool z, bool w)
 
 bool nand5(bool x, bool y, bool z, bool w, bool a)
 {
-   !((x && y) && (z && w) && a ); // 4 input NAND gate
+   !((x && y) && (z && w) && a ); // 5 input NAND gate
 }
 
 
@@ -30,15 +30,10 @@ int main()
    bool term1, term2, term3, term4, term5; // These terms are for readability
 
    term1 = nand2(nand2(Y,Y),nand2(Z,Z));
-   printf("term1 F = %x\n" ,term1);
    term2 = nand2(X,nand2(Y,Y));
-   printf("term2 F = %x\n" ,term2);
    term3 = nand3(X,nand2(Z,Z),nand2(W,W));
-   printf("term3 F = %x\n" ,term3);
    term4 = nand3(X,Z,W);
-   printf("term4 F = %x\n" ,term4);
    term5 = nand4(nand2(X,X),Y,Z,nand2(W,W));
-   printf("term5 F = %x\n" ,term5);
 
    F = nand5(term1,term2,term3,term4,term5);
 
